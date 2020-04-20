ed77365396e1717144d10df7f8349e3b02bb2f53
Julia Fernandez <jaf765@nyu.edu>
2020-04-20 14:32:25 -0400

    adds jungle animation

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
- <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+//switch div function
+
+$("btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".second").offset().top},
+        'slow');
+});
+
+//text animation
+
+var textWrapper = document.querySelector('.ml9 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml9 .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i+1)
+  }).add({
+    targets: '.ml9',
+  });
